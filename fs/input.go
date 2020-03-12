445d823ec84a8d239ae638128905cf1ba51f3b92
halfwit <michaelmisch1985@gmail.com>
2020-03-12 12:48:50 -0700

    Start work on mock input

--- fs/input.go
+++ fs/input.go
@@ -16,12 +16,19 @@ type Handler interface {
 	Handle(path string, c *markup.Lexer) error
 }
 
+type inputter interface {
+	addErr(format string, args ...interface{})
+	errs() []error
+	stop(context.Context)
+}
+
 type Input struct {
 	h     Handler
-	r     *reader
-	ew    *os.File
-	buff  string
+	r     io.ReadCloser
+	run   inputter
 	fname string
+	buff  string
+	ctx   context.Context
 	debug func(inputMsg, ...interface{})
 }
 
@@ -42,22 +49,27 @@ func NewInput(h Handler, dir, buffer string, debug bool) (*Input, error) {
 	}
 
 	inpath := path.Join(dir, buffer)
+	fp := path.Join(inpath, "input")
 
-	if _, e := os.Stat(path.Join(inpath, "input")); os.IsNotExist(e) {
-		r, err := newReader(path.Join(inpath, "input"))
+	if _, e := os.Stat(fp); os.IsNotExist(e) {
+
+		r, err := newReader(fp)
 		if err != nil {
 			return nil, err
 		}
 
-		ep := path.Join(dir, "errors")
-
-		ew, err := os.OpenFile(ep, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+		ew, err := os.OpenFile(path.Join(dir, "errors"), os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
 		if err != nil {
 			return nil, err
+		}
+
+		run := &input{
+			ew: ew,
+			fp: fp,
 		}
 
 		i := &Input{
-			ew:    ew,
+			run:   run,
 			h:     h,
 			r:     r,
 			fname: inpath,
@@ -75,10 +87,33 @@ func NewInput(h Handler, dir, buffer string, debug bool) (*Input, error) {
 	return nil, fmt.Errorf("Input file already exist at %s", inpath)
 }
 
+// NewMockInput returns a faked input file for testing, using reqs instead of a real file
+func NewMockInput(h Handler, buffer string, debug bool, reqs chan string) (*Input, error) {
+
+	mci := &mockinput{
+		reqs: reqs,
+	}
+
+	i := &Input{
+		h:     h,
+		r:     mci,
+		run:   mci,
+		fname: buffer,
+		buff:  buffer,
+		debug: func(inputMsg, ...interface{}) {},
+	}
+
+	if debug {
+		i.debug = inputLogging
+	}
+
+	return i, nil
+}
+
 // Start will watch for reads on Input's path, and send messages to the callers Handle function
 // Errors will be logged to the errors file
 func (i *Input) Start() {
-	i.StartContext(context.TODO())
+	i.StartContext(context.Background())
 }
 
 // StartContext is a variant of Start which takes a context for cancellation
@@ -87,9 +122,10 @@ func (i *Input) StartContext(ctx context.Context) {
 	errors := make(chan error)
 
 	i.debug(inputnorm, i.fname, "starting input")
+	i.ctx = ctx
 
 	go func() {
-		for msg := range inputs {
+		for msg := range inputs {		
 			l := markup.NewLexer(msg)
 			if e := i.h.Handle(i.buff, l); e != nil {
 				errors <- e
@@ -99,7 +135,6 @@ func (i *Input) StartContext(ctx context.Context) {
 
 	go func() {
 		scanner := bufio.NewScanner(i.r)
-		defer i.ew.Close()
 		defer close(inputs)
 
 		for scanner.Scan() {
@@ -109,16 +144,26 @@ func (i *Input) StartContext(ctx context.Context) {
 				return
 			case err := <-errors:
 				i.debug(inputerr, i.fname, err)
-				fmt.Fprintf(i.ew, "input error on %s: %v", i.fname, err)
+				i.run.addErr("input error on %s: %v", i.fname, err)
 			case inputs <- scanner.Bytes():
 				i.debug(inputnorm, i.fname, scanner.Bytes())
 			}
 		}
 
 		if e := scanner.Err(); e != io.EOF && e != nil {
-			fmt.Fprintf(i.ew, "input error on %s: %v", i.fname, e)
+			i.run.addErr("input error: %s: %v", i.fname, e)
 		}
 	}()
+}
+
+// Stop ends the Input session, cleaning up after itself
+func (i *Input) Stop() {
+	i.run.stop(i.ctx)
+}
+
+// Errs returns any errors encountered during input's runtime
+func (i *Input) Errs() []error {
+	return i.run.errs()
 }
 
 func inputLogging(msg inputMsg, args ...interface{}) {
---
+++ fs/input_mock.go
@@ -0,0 +1,38 @@
+package fs
+
+import (
+	"context"
+	"fmt"
+	"io"
+)
+
+type mockinput struct {
+	reqs    chan string
+	errlist []error
+}
+
+func (i *mockinput) errs() []error {
+	return i.errlist
+}
+
+func (i *mockinput) addErr(format string, args ...interface{}) {
+	e := fmt.Errorf(format, args...)
+	i.errlist = append(i.errlist, e)
+}
+
+func (i *mockinput) stop(ctx context.Context) {
+	ctx.Done()
+}
+
+func (i *mockinput) Read(b []byte) (n int, err error) {
+	for line := range i.reqs {
+		n = copy(b, []byte(line))
+		return
+	}
+
+	return 0, io.EOF
+}
+
+func (i *mockinput) Close() error {
+	return nil
+}
