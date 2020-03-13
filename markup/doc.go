47ae964f632e41473362ceceb9760eff81c89a00
halfwit <michaelmisch1985@gmail.com>
2020-03-13 11:21:27 -0700

    Update documentation

--- markup/doc.go
+++ markup/doc.go
@@ -1,19 +1,7 @@
 /*
-Package markup implements a two APIs for escaping markdown elements in raw text input.
-
-The first API is the Normal Text API, and it revolves around the type Cleaner. It is used to convert normal text into properly formatted Altid markdown.
-
-The second API is the HTML API, and it revolves around the type HTMLCleaner. It is used to convert HTML into properly formatted Altid markdown.
-
-Normal Text API
+Package markup implements an API for escaping markdown elements in raw text input.
 
 Cleaner wraps a WriteCloser, and wraps common functions to write properly formatted markdown to the Writer - these are the *Escaped variant functions. Cleaner also provides the generic methods "Write" and "WriteString", to write unmodified text to the underlying WriteCloser.
-
-HTML API
-
-HTMLCleaner wraps a WriteCloser, and is used to parse html streams into their respective Altid markdown interpretations. HTMLCleaner also provides the generic methods "Write" and "WriteString", to write unmodified text to the underlying WriteCloser.
-
-Markdown
 
 Altid-flavored markdown is described in more detail in the official document https://altid.github.io/markdown.html
 
