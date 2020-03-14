0bcd9fa572723f14d1f09bfb8c58795150ac89bd
halfwit <michaelmisch1985@gmail.com>
2020-03-13 19:16:31 -0700

    Add in buildable fallback

---
+++ flutter/altid_client/example/android/app/src/main/kotlin/com/example/altid_client_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.altid_client_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter/altid_client/ios/Classes/AltidClientPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface AltidClientPlugin : NSObject<FlutterPlugin>
+@end
