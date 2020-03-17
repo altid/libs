27d2199db6a9f9c8f3a1569b269efc66ebc6d55f
halfwit <michaelmisch1985@gmail.com>
2020-03-16 20:55:58 -0700

    Documentation cleanup

--- config/example_test.go
+++ config/example_test.go
@@ -7,18 +7,20 @@ import (
 	"github.com/altid/libs/config"
 )
 
-func buildConfig(rw io.ReadWriter) (*config.Config, error) {
-	repl := struct {
-		Address  string `IP address to dial`
-		Password string `password for service`
-		UseSSL   bool   `Use SSL?`
-		Foo      string // Will use default
-	}{"127.0.0.1", "password", false, "bar"}
 
-	return config.Repl(rw, repl, false)
-}
 
 func ExampleRepl() {
+	buildConfig := func(rw io.ReadWriter) (*config.Config, error) {
+		repl := struct {
+			Address  string `IP address to dial`
+			Password string `password for service`
+			UseSSL   bool   `Use SSL?`
+			Foo      string // Will use default
+		}{"127.0.0.1", "password", false, "bar"}
+	
+		return config.Repl(rw, repl, false)
+	}
+	
 	if _, e := config.Mock(buildConfig, "zzyzx", false); e != nil {
 		log.Fatal(e)
 	}
