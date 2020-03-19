744252a2283d75ffdfb536c58b044ba985f7f798
halfwit <michaelmisch1985@gmail.com>
2020-03-18 17:57:17 -0700

    Add in Client lib for service management

--- service/service.go
+++
@@ -1,5 +0,0 @@
-package service
-
-// This is the main orchestration to clients
-// Just needs to keep track of active/none for us
-// Set buffer for client, and maybe a simple "is there a client on this buffer?" so we can update tabs
