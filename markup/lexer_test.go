dc5cfb2bb5288c77f24d33779cec80ed9f539e57
halfwit <michaelmisch1985@gmail.com>
2020-03-28 19:16:05 -0700

    #13 work, finish commands

--- markup/lexer_test.go
+++ markup/lexer_test.go
@@ -1,11 +1,38 @@
 package markup_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/altid/libs/markup"
 	fuzz "github.com/google/gofuzz"
 )
+
+func TestSpecial(t *testing.T) {
+	myData := []byte("A string with an un-escaped hyphen")
+	l := markup.NewLexer(myData)
+
+	if _, e := l.String(); e == nil {
+		t.Error("no error on incorrectly formatted text")
+	}
+
+	myData = []byte("A string with an un\\-un\\-escaped hypthen")
+	l = markup.NewLexer(myData)
+
+	if _, e := l.String(); e != nil {
+		t.Error(e)
+	}
+
+	myData = []byte("A %[Test in a sub-tag](red) should fail")
+	l = markup.NewLexer(myData)
+
+	s, e := l.String()
+	if e == nil {
+		t.Error("no error on incorrectly formatted text")
+	}
+
+	fmt.Println(s)
+}
 
 func TestLexer(t *testing.T) {
 	for i := 0; i < 50000; i++ {
@@ -17,7 +44,7 @@ func TestLexer(t *testing.T) {
 
 		for {
 			i := l.Next()
-			
+
 			switch i.ItemType {
 			case markup.EOF:
 				return
