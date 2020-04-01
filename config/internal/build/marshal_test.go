ceb4e72ae249ad6d944ce1fb8355da34f8ebbd16
halfwit <michaelmisch1985@gmail.com>
2020-03-31 19:45:37 -0700

    Let's get dangerous

--- config/internal/build/marshal_test.go
+++ config/internal/build/marshal_test.go
@@ -8,16 +8,32 @@ import (
 
 func TestPush(t *testing.T) {
 	test := &struct {
-		Name string `altid:"Some string"`
-		Port int    `altid:",omit_empty"`
-	}{"None", 0}
+		Name string `altid:"name,prompt=Some string,pick=foo|bar|baz"`
+		Port int    `altid:"chicken,omit_empty"`
+		Banana bool
+	}{"foo", 0, false}
 
 	entry := &entry.Entry{
-		Key:   "Name",
-		Value: "none",
+		Key:   "name",
+		Value: "bar",
 	}
 
 	if e := push(test, entry); e != nil {
 		t.Error(e)
+	}
+
+	if test.Name != "bar" {
+		t.Error("was unable to set entry value")
+	}
+
+	entry.Key = "chicken"
+	entry.Value = 26
+
+	if e := push(test, entry); e != nil {
+		t.Error(e)
+	}
+
+	if test.Port != 26 {
+		t.Error("was unable to set entry value")
 	}
 }
--- config/internal/request/request_test.go
+++ config/internal/request/request_test.go
@@ -1,6 +1,7 @@
 package request
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/altid/libs/config/types"
@@ -9,32 +10,36 @@ import (
 func TestRequest(t *testing.T) {
 	// Create and marshal a struct, make sure we're good here
 	test := &struct {
-		Auth   types.Auth `A tag`
-		Log    types.Logdir
-		Listen types.ListenAddress
-		Name   string `Another tag`
-		Port   int32
-		Debug  bool
-	}{"none", "none", "none", "halfwit", 1234, true}
+		Auth   types.Auth          `altid:"auth,prompt:Authentication method,pick:password|factotum|none"`
+		Log    types.Logdir        `altid:"logdir,no_prompt"`
+		Listen types.ListenAddress `altid:"listen_address,no_prompt"`
+		Name   string              `altid:"username,prompt:Name to use for service"`
+		Port   int32               `altid:"port,prompt:Port to connect on"`
+		Debug  bool                `xml:"debug,omit_empty"`
+	}{"password", "none", "none", "halfwit", 1234, true}
 
 	reqs, err := Build(test)
 	if err != nil {
 		t.Error(err)
 	}
 
-	if len(reqs) != 6 {
+	for i := 0; i < 5; i++ {
+		fmt.Println(reqs[i])
+	}
+
+	if len(reqs) != 5 {
 		t.Error("incorrect number of requests found")
 	}
 
-	if reqs[0].Prompt != "A tag" {
+	if len(reqs[0].Pick) != 3 {
+		t.Error("unable to find picks for auth")
+	}
+
+	if reqs[0].Prompt == "" {
 		t.Error("was unable to set prompt for Auth")
 	}
 
 	if reqs[4].Defaults.(int32) != 1234 {
 		t.Error("was unable to set int type")
-	}
-
-	if reqs[5].Defaults.(bool) != true {
-		t.Error("was unable to set bool type")
 	}
 }
