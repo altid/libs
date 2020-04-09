04c910335b2eaf83c31464889272788ceb9400b4
halfwit <michaelmisch1985@gmail.com>
2020-04-08 21:04:37 -0700

    Stupidity in merges happens. Thanks VSCODE

--- internal/command/command.go
+++ internal/command/command.go
@@ -13,12 +13,6 @@ type CmdType int
 const (
 	OtherCmd CmdType = iota
 	OpenCmd
-<<<<<<< HEAD:command/command.go
-	ReloadCmd
-	RestartCmd
-	BufferCmd
-=======
->>>>>>> dev:internal/command/command.go
 	CloseCmd
 	BufferCmd
 	LinkCmd
@@ -30,16 +24,6 @@ const (
 // There are more possible commands, but these are the only ones
 // the server itself intercepts in any meaningful way
 var items = map[string]CmdType{
-<<<<<<< HEAD:command/command.go
-	"open":    OpenCmd,
-	"close":   CloseCmd,
-	"buffer":  BufferCmd,
-	"restart": RestartCmd,
-	"link":    LinkCmd,
-	"quit":    QuitCmd,
-	"reload":  ReloadCmd,
-	"other":   OtherCmd,
-=======
 	"other":   OtherCmd,
 	"open":    OpenCmd,
 	"close":   CloseCmd,
@@ -48,7 +32,6 @@ var items = map[string]CmdType{
 	"quit":    QuitCmd,
 	"reload":  ReloadCmd,
 	"restart": RestartCmd,
->>>>>>> dev:internal/command/command.go
 }
 
 // Command is a ctl message
@@ -56,48 +39,16 @@ type Command struct {
 	// The sending client ID
 	UUID    uint32
 	CmdType CmdType
-<<<<<<< HEAD:command/command.go
-	// The buffer that the command came from
-	From string
-	// The arguments to the command
-	Args string
-	// The raw command bytes
-	Data []byte
-=======
 	// Arguments for known commands, such as buffer/open/close/link
 	Args []string
 	// The active buffer of the client who sent the command
 	From string
->>>>>>> dev:internal/command/command.go
 }
 
 // New - helper func with varargs
 // If there are 2 args, the first is assumed to be the "From" path, the second "Args"
 // If only one, it is set as "Args"
 //
-<<<<<<< HEAD:command/command.go
-//		cmdChannel <- New(clientuuid, BufferCmd, nil, "oldbuffer", "newbuffer")
-//		cmdChannel <- New(clientuuid, CloseCmd, nil, "oldbuffer")
-//		cmdChannel <- New(clientuuid, OtherCmd, []byte("emote I swapped buffers"))
-//		cmdChannel <- New(clientuuid, LinkCmd, nil, "newbuffer", "oldbuffer")
-//
-func New(uuid uint32, cmdType CmdType, data []byte, args ...string) *Command {
-	c := &Command{
-		UUID:    uuid,
-		CmdType: cmdType,
-		Data:    data,
-	}
-
-	switch len(args) {
-	case 1:
-		c.Args = args[0]
-	case 2:
-		c.From = args[0]
-		c.Args = args[1]
-	}
-
-	return c
-=======
 //		cmdChannel <- New(clientuuid, BufferCmd, "somebuffer", "newbuffer")
 //		cmdChannel <- New(clientuuid, CloseCmd, "somebuffer", "oldbuffer")
 //		cmdChannel <- New(clientuuid, LinkCmd, "oldbuffer", "newbuffer")
@@ -144,7 +95,6 @@ func FromString(uuid uint32, from, s string) (*Command, error) {
 	}
 
 	return c, nil
->>>>>>> dev:internal/command/command.go
 }
 
 // WriteOut will write the command correctly to the Writer
@@ -153,27 +103,16 @@ func (c *Command) WriteOut(w io.Writer) error {
 	// check the args count before writing
 	switch c.CmdType {
 	case OpenCmd:
-<<<<<<< HEAD:command/command.go
-		fmt.Fprintf(w, "open \"%s\" \"%s\"\n", c.From, c.Args)
-=======
 		if len(c.Args) != 1 {
 			return errors.New("incorrect arguments to open")
 		}
 
 		fmt.Fprintf(w, "open %s %s\n", c.From, c.Args[0])
->>>>>>> dev:internal/command/command.go
 	case RestartCmd:
 		return errors.New("restart command not writable. Did you mean service restart?")
 	case ReloadCmd:
 		return errors.New("reload command not writable. Did you mean service reload?")
 	case BufferCmd:
-<<<<<<< HEAD:command/command.go
-		fmt.Fprintf(w, "buffer \"%s\" \"%s\"\n", c.From, c.Args)
-	case CloseCmd:
-		fmt.Fprintf(w, "close \"%s\"\n", c.Args)
-	case LinkCmd:
-		fmt.Fprintf(w, "link \"%s\" \"%s\"\n", c.From, c.Args)
-=======
 		if len(c.Args) != 1 {
 			return errors.New("incorrect arguments to buffer")
 		}
@@ -191,7 +130,6 @@ func (c *Command) WriteOut(w io.Writer) error {
 		}
 
 		fmt.Fprintf(w, "link %s %s\n", c.From, c.Args[0])
->>>>>>> dev:internal/command/command.go
 	case QuitCmd:
 		fmt.Fprintln(w, "quit")
 	default:
--- internal/command/command_test.go
+++ internal/command/command_test.go
@@ -1,30 +1,10 @@
 package command
 
 import (
-<<<<<<< HEAD:command/command_test.go
-	"bufio"
-	"bytes"
-=======
->>>>>>> dev:internal/command/command_test.go
 	"io/ioutil"
 	"testing"
 )
 
-<<<<<<< HEAD:command/command_test.go
-func TestWriteOut(t *testing.T) {
-	var data bytes.Buffer
-	test := bufio.NewWriter(&data)
-
-	a := New(0, LinkCmd, nil, "foo", "bar")
-	if e := a.WriteOut(test); e != nil {
-		t.Error(e)
-	}
-
-	test.Flush()
-
-	if data.String() != `link "foo" "bar"`+"\n" {
-		t.Error("Incorrect link output")
-=======
 func TestFromBytes(t *testing.T) {
 	if c, e := FromBytes(0, "mybuffer", []byte("open foo")); e != nil || c.Args[0] != "foo" {
 		t.Errorf("from bytes failed on open foo %v", e)
@@ -52,23 +32,8 @@ func TestFromBytes(t *testing.T) {
 
 	if c, e := FromBytes(0, "mybuffer", []byte("some other command")); e != nil || c.CmdType != OtherCmd {
 		t.Errorf("from bytes failed on other test %v", e)
->>>>>>> dev:internal/command/command_test.go
 	}
 
-<<<<<<< HEAD:command/command_test.go
-	data.Reset()
-	b := New(0, OpenCmd, nil, "foo", "bar")
-	if e := b.WriteOut(test); e != nil {
-		t.Error(e)
-	}
-
-	test.Flush()
-	if data.String() != `open "foo" "bar"`+"\n" {
-		t.Error("Incorrect open output")
-	}
-
-	c := New(0, CloseCmd, nil, "foo", "bar")
-=======
 func TestWriteOut(t *testing.T) {
 	a := New(0, LinkCmd, "foo", "bar")
 	if e := a.WriteOut(ioutil.Discard); e != nil {
@@ -81,41 +46,21 @@ func TestWriteOut(t *testing.T) {
 	}
 
 	c := New(0, CloseCmd, "bar", "foo")
->>>>>>> dev:internal/command/command_test.go
 	if e := c.WriteOut(ioutil.Discard); e != nil {
 		t.Error(e)
 	}
 
-<<<<<<< HEAD:command/command_test.go
-	d := New(0, BufferCmd, nil, "foo", "bar")
-=======
 	d := New(0, BufferCmd, "foo", "bar")
->>>>>>> dev:internal/command/command_test.go
 	if e := d.WriteOut(ioutil.Discard); e != nil {
 		t.Error(e)
 	}
 
-<<<<<<< HEAD:command/command_test.go
-	data.Reset()
-	f := New(0, QuitCmd, nil)
-	if e := f.WriteOut(test); e != nil {
-		t.Error(e)
-	}
-
-	test.Flush()
-	if data.String() != `quit`+"\n" {
-		t.Error("quit command improperly formatted")
-	}
-
-	g := New(0, ReloadCmd, nil)
-=======
 	f := New(0, QuitCmd, "foo")
 	if e := f.WriteOut(ioutil.Discard); e != nil {
 		t.Error(e)
 	}
 
 	g := New(0, ReloadCmd, "foo")
->>>>>>> dev:internal/command/command_test.go
 	if e := g.WriteOut(ioutil.Discard); e == nil {
 		t.Error("reload should return error on WriteOut")
 	}
