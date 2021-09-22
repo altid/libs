d43fc0d3ca5478ede4c9140a0416ab78208c0a86
Michael Misch <michealmisch1985@gmail.com>
2021-09-21 20:45:37 -0700

    Move around things, remove other things

--- server/doc.go
+++
@@ -1,6 +0,0 @@
-/*Package server is a library used to provide server capabilities to Altid services 
-
-	go get github.com/altid/server
-
- */
-package server
--- server/internal/tabs/doc.go
+++
@@ -1,7 +0,0 @@
-/*Package tabs contains helper functions for managing tabs in Altid services
-
-	go get github.com/altid/server/tabs
-
-
-*/
-package tabs
