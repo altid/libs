c306ad7ff09dab6416a5a3ca913b8bcfff96838d
halfwit <michaelmisch1985@gmail.com>
2023-05-15 22:01:29 -0700

    Use a Dir interface for our returned type with access to a correct fileinfo. This allows listing and the rest of the actions to work correctly

--- store/ramstore_test.go
+++ store/ramstore_test.go
@@ -2,6 +2,7 @@ package store
 
 import (
 	"io"
+	"reflect"
 	"testing"
 )
 
@@ -10,27 +11,60 @@ func TestRamstore(t *testing.T) {
 	if e := rs.Mkdir("/chicken"); e != nil {
 		t.Error(e)
 	}
-	_, err := rs.Open("/chicken/cluck")
+	f, err := rs.Open("/chicken/cluck")
 	if err != nil {
 		t.Error(err)
 	}
-
-	f, err := rs.Open("/check")
+	f.Write([]byte("Test data"))
+	f.Close()
+	f, err = rs.Open("/chicken/cluck")
 	if err != nil {
 		t.Error(err)
 	}
-	if _, e := f.Write([]byte("Hello")); e != nil {
+	f.Seek(0, io.SeekEnd)
+	if _, e := f.Write([]byte(" Hello")); e != nil {
 		t.Error(e)
 	}
 	if _, e := f.Seek(0, io.SeekStart); e != nil {
 		t.Error(e)
 	}
-	b := make([]byte, 5)
+	b := make([]byte, 15)
 	if _, e := f.Read(b); e != nil && e != io.EOF {
 		t.Error(e)
 	}
-	if string(b) != "Hello" {
+	if string(b) != "Test data Hello" {
 		t.Error("Strings do not match")
 	}
-	//f.Close()
+	if f.Name() != "/chicken/cluck" {
+		t.Error("Expected file name different from actual")
+	}
+	f.Close()
 }
+
+func TestRamstoreList(t *testing.T) {
+	// Array to test against
+	n := []string{"/ptarmigan/leg", "/swan"}
+
+	rs := NewRamstore(true)
+	if e := rs.Mkdir("/chicken"); e != nil {
+		t.Error(e)
+	}
+	if e := rs.Mkdir("/duck"); e != nil {
+		t.Error(e)
+	}
+	if e := rs.Mkdir("/ptarmigan"); e != nil {
+		t.Error(e)
+	}
+	if _, e := rs.Open("/ptarmigan/leg"); e != nil {
+		t.Error(e)
+	}
+	if _, e := rs.Open("/swan"); e != nil {
+		t.Error(e)
+	}
+	l := rs.List()
+	if ! reflect.DeepEqual(l, n) {
+		t.Error("string arrays do not match")
+	}
+	t.Log(l, n)
+}
+
