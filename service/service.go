1c40fb174e99c1a08a65e9d3d2dea892cde46d74
halfwit <michaelmisch1985@gmail.com>
2024-07-09 22:28:00 -0700

    Wire up as much of the control schema as we can

--- service/service.go
+++ service/service.go
@@ -1,17 +1,36 @@
 package service
 
 import (
+	"context"
+
+	"github.com/altid/libs/service/callback"
 	"github.com/altid/libs/service/commander"
-	"github.com/altid/libs/service/controller"
 	"github.com/altid/libs/service/internal/control"
 )
 
-func Start(name string, cmd func(*commander.Command)) (controller.Controller, error) {
-	ctl, err := control.ConnectService(name)
+type Service struct {
+	ctl *control.Control
+}
+
+func Register(ctx context.Context, name string) (*Service, error) {
+	ctl, err := control.ConnectService(ctx, name)
 	if err != nil {
 		return nil, err
 	}
 
-	go ctl.Listen(cmd)
-	return ctl, nil
+	return &Service{
+		ctl: ctl,
+	}, nil
+}
+
+func (s *Service) SetCommands(cmds []*commander.Command) {
+	s.ctl.SetCommands(cmds)
+}
+
+func (s *Service) SetCallbacks(cb callback.Callback) {
+	s.ctl.WithCallbacks(cb)
+}
+
+func (s *Service) Listen() error {
+	return s.ctl.Listen()
 }
